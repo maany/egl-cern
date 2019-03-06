505b723bb5c0ba79b5afb9dd1a070a41d0050777
maany <imptodefeat@gmail.com>
2019-03-06 17:41:02 +0100

    #1 sites endpoint is not /rest/sites

--- egl_rest/views.py
+++ egl_rest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import json
 
 cms_cric_url = "https://cms-cric.cern.ch/api/core/rcsite/query/?json"
 wlcg_cric_url = "https://wlcg-cric.cern.ch/api/core/rcsite/query/?json"
@@ -8,4 +9,6 @@ wlcg_cric_url = "https://wlcg-cric.cern.ch/api/core/rcsite/query/?json"
 # Create your views here.
 def index(request):
     output = requests.get(cms_cric_url, verify = False)
+    #data = json.loads(output.text)
+    #print(data)
     return HttpResponse(output.text)
