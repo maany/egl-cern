35f8bb93baec652f216308bd6d6b978e51b1170e
maany <imptodefeat@gmail.com>
2019-04-03 13:32:18 +0200

    #4 add initial fields for Site

--- egl_rest/models.py
+++ egl_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+class Site(models.Model):
+    name = models.CharField()
+    tier = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    country_code = models.CharField()
+    country = models.CharField()
+    supported_vos
+    pass
+
+class Pledge(models.Model):
+    pass
+
+class Federation(models.Model):
+    pass
