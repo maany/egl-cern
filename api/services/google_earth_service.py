24597c4bde3c19aeb3e03e53615a9486e19d44d9
maany <imptodefeat@gmail.com>
2019-06-04 20:23:02 +0200

    backup

--- api/services/google_earth_service.py
+++ api/services/google_earth_service.py
@@ -1,13 +1,15 @@
 from api.event_hub.event_managers import IEGLEventListener
 from api.event_hub.events.data_fetch_parse_events import NewDataAvailableOnlineEvent
+from api.event_hub import EventHub
+
 
 class GoogleEarthService(IEGLEventListener):
 
     def __init__(self):
-        
-    @classmethod
-    def process_kml(cls):
-        pass
+        EventHub.register_listener(self, NewDataAvailableOnlineEvent)
+
+    def process_kml(self):
+        print("Processing KML")
 
     def notify(self, egl_event):
-        if egl_event.sequence contain
+        self.process_kml()
