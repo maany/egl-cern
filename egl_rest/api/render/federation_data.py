d7ecfbc6da094fdf92ecb9ecfa024c35550790da
maany <imptodefeat@gmail.com>
2019-07-08 18:56:03 +0200

    add pledge info for federations

--- egl_rest/api/render/federation_data.py
+++ egl_rest/api/render/federation_data.py
@@ -14,14 +14,37 @@ class FederationData(Singleton):
         for federation in federations:
             supported_vos = []
             sites = []
+            pledges = []
             for vo in federation.supported_vos.all():
                 supported_vos.append(vo.name)
             for site in federation.site_set.filter(active=True):
                 sites.append(site.name)
+            for pledge in federation.pledge_set.all():
+                if pledge.tape is -1:
+                    tape = "Not Specified"
+                else:
+                    tape = pledge.tape
+                if pledge.cpu is -1:
+                    cpu = "Not Specified"
+                else:
+                    cpu = pledge.cpu
+                if pledge.disk is -1:
+                    disk = "Not Specified"
+                else:
+                    disk = pledge.disk
+
+                pledges.append({
+                    pledge.year: {
+                        "cpu": cpu,
+                        "disk": disk,
+                        "tape": tape
+                    }
+                })
             federations_dict[federation.name] = {
                 "accounting_name": federation.accounting_name,
                 "supported_vos": supported_vos,
-                "sites": sites
+                "sites": sites,
+                "pledges": pledges
             }
         json_rep = json.dumps(federations_dict)
         return json_rep
